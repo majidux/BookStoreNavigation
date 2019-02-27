7f3849fe50f7928554e9716639323688ca08b02c
m.darvishnejad <majid>
2019-02-27 17:12:58 +0330

    Add login

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,20 +1,32 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Dimensions} from 'react-native';
 
-
+let deviceWidth = Dimensions.get('window').width;
 export default class Login extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text>Home</Text>
+                <View style={styles.blueView}></View>
+                <Text>klmf;sdnf;sdnfs;adjlfn</Text>
+                <View>
+                    <Text>Home</Text>
+                </View>
             </View>
         );
     }
 }
 // const RootStack =
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        backgroundColor:'#e45'
-    }
+    container: {
+        flex: 1,
+        backgroundColor: '#e45'
+    },
+    blueView: {
+        width: deviceWidth * 4,
+        height: 150,
+        backgroundColor: '#206bec',
+        transform: [
+            {rotateY: '260deg'}
+        ],
+    },
 });
